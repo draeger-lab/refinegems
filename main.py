a2e2dd77c6859736edb6c5c715166c586692c258
famosab <famke.baeuerle@gmail.com>
2021-06-06 18:40:03 +0200

    test multiple media

--- main.py
+++ main.py
@@ -12,17 +12,16 @@ def main():
     
     with open('config.yaml') as f:
         config = yaml.safe_load(f)
-    globals().update(config) # converts settings from config.yaml to variables
 
-    if (keggpathways != None):
-        rg.kegg_pathways(input, keggpathways)
-        model, errors = cobra.io.sbml.validate_sbml_model(keggpathways)
+    if (config['keggpathways'] != None):
+        rg.kegg_pathways(config['model'], config['keggpathways'])
+        model, errors = cobra.io.sbml.validate_sbml_model(config['keggpathways'])
     
     else:
-        model_cobra, errors = cobra.io.sbml.validate_sbml_model(input)
+        model_cobra, errors = cobra.io.sbml.validate_sbml_model(config['model'])
 
         if (model_cobra != None):
-            model_libsbml = rg.load_model_libsbml(input)
+            model_libsbml = rg.load_model_libsbml(config['model'])
             name, reac, metab, genes = rg.initial_analysis(model_libsbml)
             print('Name: ' + name)
             print('# reactions: ' + str(reac))
@@ -30,15 +29,17 @@ def main():
             print('# genes: ' + str(genes))
             #score = rg.run_memote(model_cobra)
             #print('Memote score: ' + str(score))
-            if (medium != None):
-                essential, missing, growth, dt = rg.growth_simulation(model_cobra, model_libsbml, medium, mediumname)
-                print('Growth was tested on ' + mediumname)
-                print('The following exchanges are needed for growth and not in the medium:')
-                print(essential)
-                print('With them the growth value is ' + str("%.2f" %growth) + ' mmol/gDW·h')
-                print('The corresponding doubling time is ' + str("%.2f" %dt)+ ' min')
-                print('The following exchanges are in the medium but not in the model:')
-                print(missing)
+            if (config['media_db'] != None):
+                for medium in config['media']:
+                    essential, missing, growth, dt = rg.growth_simulation(model_cobra, model_libsbml, config['media_db'], medium)
+                    print('---')
+                    print('Growth was tested on ' + medium)
+                    print('The following exchanges are needed for growth and not in the medium:')
+                    print(essential)
+                    print('With them the growth value is ' + str("%.2f" %growth) + ' mmol/gDW·h')
+                    print('The corresponding doubling time is ' + str("%.2f" %dt)+ ' min')
+                    print('The following exchanges are in the medium but not in the model:')
+                    print(missing)
 
 
 if __name__ == "__main__":
