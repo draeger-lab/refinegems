2c8bc6200577a66f7b1f55ad7cdf7a78daae3227
famosab <famke.baeuerle@gmail.com>
2021-07-07 15:12:25 +0200

    [ADD] refinegems to Pipfile

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='refinegems',
+      version='1.0',
+      description='Scripts to curate a GEM',
+      author='Famke Baeuerle',
+      author_email='famke.baeuerle@gmail.com',
+      license='MIT',
+      packages=['refinegems'],
+      zip_safe=False)
