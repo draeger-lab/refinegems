57b0810e1085aaef995ac2d680c249777ab14888
famosab <famke.baeuerle@gmail.com>
2022-11-03 16:34:54 +0100

    change init to include all functions #27

--- refinegems/__init__.py
+++ refinegems/__init__.py
@@ -3,12 +3,12 @@
 from refinegems.growth import get_all_minimum_essential, get_growth_selected_media
 from refinegems.genecomp import genecomp
 from refinegems.modelseed import modelseed
-from refinegems.cvterms import parse_id_from_cv_term
 from refinegems.comparison import simulate_all
 import refinegems.sboann
 import refinegems.charges
 import refinegems.pathways
 import refinegems.curate
 import refinegems.polish
+import refinegems.cvterms
 
 __author__ = "Famke Baeuerle"
