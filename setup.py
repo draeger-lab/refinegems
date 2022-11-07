431419df130dbfbe319bf00b648c801695e8264c
famosab <famke.baeuerle@gmail.com>
2022-11-07 09:28:19 +0100

    solve markupsafe and depinfo #37

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
 # needed for installation of refinegems
 from setuptools import setup
 
-setup(name='refinegems',
+setup(name='refineGEMs',
       version='1.2',
       description='Scripts to curate a GEM',
       author='Famke Baeuerle',
       author_email='famke.baeuerle@gmail.com',
       license='MIT',
       packages=['refinegems'],
-      install_requires = ["cobra==0.22.0",
+      install_requires = [
+            "cobra==0.22.0",
             "biopython==1.79",
-            "bioservices==1.7.11",
-            "memote==0.11.1",
-            "escher==1.7.3",
+            "bioservices",
+            "memote==0.13.0",
             "pandas==1.2.4",
             "numpy==1.20.3",
             "pyyaml==5.4.1",
-            "psycopg2-binary==2.9.1",
-            "gffutils==0.10.1"],
+            "gffutils==0.10.1",
+            "markupsafe==2.0.1", 
+            "depinfo==1.7.0"
+            ],
       zip_safe=False)
-
-# switching to setuptools 57.5.0 fixed the use_2to3 error of installation of suds_jurko
-# help(function) shows docstring of said function (use q to quit)
-# python3 -m build
-# twine upload --repository testpypi dist/*
-# always change version before uploading / remove old dist
