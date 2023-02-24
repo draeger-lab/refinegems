9721f62623da8250ba6be054f6189758e6d07346
GwennyGit <81755070+GwennyGit@users.noreply.github.com>
2023-02-24 16:12:11 +0100

    Changed output format for gapfill #52

--- refinegems/gapfill.py
+++ refinegems/gapfill.py
@@ -2,7 +2,7 @@
 from libsbml import *
 import refinegems.analysis_kegg as rga_kegg
 import refinegems.analysis_biocyc as rga_biocyc
-from pandas import DataFrame
+import pandas as pd
 from typing import Union
 from colorama import init as colorama_init
 from colorama import Fore
@@ -22,13 +22,14 @@ def gff_gene_comp():
     pass
 
 
-def gapfill_analysis(model_libsbml: Model, gapfill_params: dict[str: str]) -> Union[DataFrame, tuple]:
+def gapfill_analysis(model_libsbml: Model, gapfill_params: dict[str: str], filename: str) -> Union[pd.DataFrame, tuple]:
     """Main function to infer gaps in a model by comparing the locus tags of the GeneProducts 
         to KEGG/BioCyc/(Genbank) GFF file
    
         Args:
             model_libsbml (Model): model loaded with libSBML
             gapfill_params (dict): Dictionary obtained from YAML file containing the parameter mappings
+            filename (str): Path to output file for BioCyc
             
         Returns:
             Case 'KEGG' - A data frame containing the columns 'bigg_id' 'locus_tag' 'EC' 'KEGG' 'name' 'GPR'
@@ -48,6 +49,7 @@ def gapfill_analysis(model_libsbml: Model, gapfill_params: dict[str: str]) -> Un
     """
     colorama_init(autoreset=True)
     db_to_compare = gapfill_params['db_to_compare']
+    result = None
     
     if db_to_compare not in ['KEGG', 'BioCyc', 'GFF', 'KEGG+BioCyc']:
         print(f'{Fore.RED}To use the module gapfill the parameter of db_to_compare has to be set to one of the following' 
@@ -63,7 +65,7 @@ def gapfill_analysis(model_libsbml: Model, gapfill_params: dict[str: str]) -> Un
                                                    gapfill_params['organismid'], 
                                                    gapfill_params['gff_file']
                                                    )
-            return missing_kegg
+            result = missing_kegg
         else:
             print(f'{Fore.RED}To use the KEGG comparison the specification of the organismid is obligatory.\n' +
                   'If there is no organismid available for your organism in KEGG, use one of the options \'BioCyc\' or \'GFF\'.')
@@ -72,13 +74,13 @@ def gapfill_analysis(model_libsbml: Model, gapfill_params: dict[str: str]) -> Un
         missing_biocyc = rga_biocyc.biocyc_gene_comp(model_libsbml, 
                                                      gapfill_params['biocyc_files']
                                                      )
-        return missing_biocyc
+        result = missing_biocyc
         
     elif db_to_compare == 'GFF':
         gff_genes = gff_gene_comp(model_libsbml, 
                                   gapfill_params['gff_file']
                                   )
-        return gff_genes
+        result = gff_genes
         
     elif db_to_compare == 'KEGG+BioCyc':
         missing_kegg_reacs = rga_kegg.kegg_gene_comp(model_libsbml, 
@@ -91,7 +93,23 @@ def gapfill_analysis(model_libsbml: Model, gapfill_params: dict[str: str]) -> Un
                                                      )
         stats, missing_biocyc_genes, missing_biocyc_metabs, missing_metabs_wo_BiGG_df, missing_biocyc_reacs = missing_biocyc
         missing_combined_reacs = missing_biocyc_reacs.merge(missing_kegg_reacs, how='left', on='bigg_id')
-        return (stats, missing_biocyc_genes, missing_biocyc_metabs, missing_metabs_wo_BiGG_df, missing_combined_reacs, missing_kegg_reacs)
+        result = (stats, missing_biocyc_genes, missing_biocyc_metabs, missing_metabs_wo_BiGG_df, missing_combined_reacs, missing_kegg_reacs)
+        
+        
+    if type(result) == tuple:
+        with pd.ExcelWriter(filename) as writer:
+            result[0].to_excel(writer, sheet_name='gap fill statistics', index=False)
+            result[1].to_excel(writer, sheet_name='genes', index=False)
+            result[2].to_excel(writer, sheet_name='metabolites', index=False)
+            result[3].to_excel(writer, sheet_name='metabolites without BiGG IDs', index=False)
+            result[4].to_excel(writer, sheet_name='reactions', index=False)
+            if len(result) == 6:
+                result[5].to_excel(writer, sheet_name='KEGG reactions', index=False)
+    else:
+        with pd.ExcelWriter(filename) as writer:
+            result.to_excel(writer, sheet_name='KEGG reactions', index=False)
+        
+    return result
     
     
 def gapfill_model(model_libsbml: Model, gapfill_analysis_result: Union[str, tuple]):
@@ -102,10 +120,14 @@ def gapfill_model(model_libsbml: Model, gapfill_analysis_result: Union[str, tupl
             gapfill_analysis_result (str | tuple): Path to Excel file from gapfill_analysis | Tuple of pandas data 
                 frames obtained from gapfill_analysis
     """
+    if type(gapfill_analysis_result) == tuple:  # Tuple of pandas dataframes from gapfill_analysis
+        pass  # Handle file
+    else:  # Excel file from user-input
+        pass  # Handle file
     pass
 
 
-def gapfill(model_libsbml: Model, gapfill_params: dict[str: str]) -> Union[DataFrame, tuple]:
+def gapfill(model_libsbml: Model, gapfill_params: dict[str: str]) -> Union[pd.DataFrame, tuple]:
     """Main function to fill gaps in a model by comparing the locus tags of the GeneProducts to 
         KEGG/BioCyc/(Genbank) GFF file
    
