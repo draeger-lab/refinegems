dad973bf35bfc35b026b6665928bbdcdadfbe527
Gwendolyn O. Gusak <81755070+GwennyGit@users.noreply.github.com>
2023-03-06 19:57:30 +0100

    Update doc strings io #74

--- refinegems/io.py
+++ refinegems/io.py
@@ -14,36 +14,38 @@ import gffutils
 import sqlalchemy
 import logging
 import pandas as pd
+from cobra import Model as cobraModel
 from ols_client import EBIClient
 from Bio import Entrez, SeqIO
 from refinegems.databases import PATH_TO_DB
-from libsbml import SBMLReader, writeSBMLToFile, Model, SBMLValidator, SBMLDocument
+from libsbml import Model as libModel
+from libsbml import SBMLReader, writeSBMLToFile, SBMLValidator, SBMLDocument
 from datetime import date
 
 __author__ = "Famke Baeuerle and Gwendolyn O. Gusak"
 
 
-def load_model_cobra(modelpath: str) -> cobra.Model:
-    """loads model using cobrapy
+def load_model_cobra(modelpath: str) -> cobraModel:
+    """Loads model using COBRApy
 
     Args:
-        modelpath (str): Path to GEM
+        - modelpath (str): Path to GEM
 
     Returns:
-        cobra-model: loaded model by cobrapy
+        cobraModel: Loaded model by COBRApy
     """
     mod = cobra.io.read_sbml_model(modelpath)
     return mod
 
 
-def load_model_libsbml(modelpath: str) -> Model:
-    """loads model using libsbml
+def load_model_libsbml(modelpath: str) -> libModel:
+    """Loads model using libSBML
 
     Args:
-        modelpath (str): Path to GEM
+        - modelpath (str): Path to GEM
 
     Returns:
-        libsbml-model: loaded model by libsbml
+        libModel: loaded model by libSBML
     """
     reader = SBMLReader()
     read = reader.readSBMLFromFile(modelpath)  # read from file
@@ -52,6 +54,15 @@ def load_model_libsbml(modelpath: str) -> Model:
 
 
 def load_multiple_models(models: list[str], package: str) -> list:
+    """Loads multiple models into a list
+
+    Args:
+        - models (list): List of paths to models
+        - package (str): COBRApy|libSBML
+
+    Returns:
+        list: List of model objects loaded with COBRApy|libSBML
+    """
     loaded_models = []
     for modelpath in models:
         if package == 'cobra':
@@ -62,13 +73,13 @@ def load_multiple_models(models: list[str], package: str) -> list:
 
 
 def load_document_libsbml(modelpath: str) -> SBMLDocument:
-    """loads model document using libsbml
+    """Loads model document using libSBML
 
     Args:
-        modelpath (str): Path to GEM
+        - modelpath (str): Path to GEM
 
     Returns:
-        libsbml-document: loaded document by libsbml
+        SBMLDocument: Loaded document by libSBML
     """
     reader = SBMLReader()
     read = reader.readSBMLFromFile(modelpath)  # read from file
@@ -79,10 +90,10 @@ def load_medium_custom(mediumpath: str) -> pd.DataFrame:
     """Helper function to read medium csv
 
     Args:
-        mediumpath (Str): path to csv file with medium
+        - mediumpath (str): path to csv file with medium
 
     Returns:
-        df: pandas dataframe of csv
+        pd.DataFrame: Table of csv
     """
     medium = pd.read_csv(mediumpath, sep=';')
     medium['BiGG_R'] = 'R_EX_' + medium['BiGG'] + '_e'
@@ -94,10 +105,10 @@ def load_medium_from_db(mediumname: str) -> pd.DataFrame:
     """Wrapper function to extract subtable for the requested medium from the database 'data.db'
 
     Args:
-        mediumname (Str): name of medium to test growth on
+        - mediumname (str): Name of medium to test growth on
 
     Returns:
-        df: pandas dataframe containing composition for one medium with metabs added as BiGG_EX exchange reactions
+        pd.DataFrame: Table containing composition for one medium with metabs added as BiGG_EX exchange reactions
     """
     medium_query = f"SELECT * FROM media m JOIN media_compositions mc ON m.id = mc.medium_id WHERE m.medium = '{mediumname}'" 
     medium = load_a_table_from_database(medium_query)
@@ -111,10 +122,10 @@ def load_all_media_from_db(mediumpath: str) -> pd.DataFrame:
     """Helper function to extract media definitions from media_db.csv
 
     Args:
-        mediumpath (Str): path to csv file with medium database
+        - mediumpath (str): Path to csv file with medium database
 
     Returns:
-        df: pandas dataframe of csv with metabs added as BiGG_EX exchange reactions
+        pd.DataFrame: Table from csv with metabs added as BiGG_EX exchange reactions
     """
     media = pd.read_csv(mediumpath, sep=';')
     media['BiGG_R'] = 'R_EX_' + media['BiGG'] + '_e'
@@ -129,14 +140,14 @@ def load_all_media_from_db(mediumpath: str) -> pd.DataFrame:
 
 
 def load_manual_annotations(tablepath: str='data/manual_curation.xlsx', sheet_name: str='metab') -> pd.DataFrame:
-    """loads metabolite sheet from manual curation table
+    """Loads metabolite sheet from manual curation table
 
     Args:
-        tablepath (str): Path to manual curation table. Defaults to 'data/manual_curation.xlsx'.
-        sheet_name (str): Sheet name for metabolite annotations. Defaults to 'metab'.
+        - tablepath (str): Path to manual curation table. Defaults to 'data/manual_curation.xlsx'.
+        - sheet_name (str): Sheet name for metabolite annotations. Defaults to 'metab'.
 
     Returns:
-        df: table as pandas df
+        pd.DataFrame: Table containing specified sheet from Excel file
     """
     man_ann = pd.read_excel(tablepath, sheet_name)
     return man_ann
@@ -147,7 +158,7 @@ def load_a_table_from_database(table_name_or_query: str) -> pd.DataFrame:
         true from the refineGEMs database ('data/database/data.db')
 
     Args:
-        table_name_or_query (str): Name of a table contained in the database 'data.db'/ a SQL query
+        - table_name_or_query (str): Name of a table contained in the database 'data.db'/ a SQL query
 
     Returns:
         pd.DataFrame: Containing the table for which the name was provided from the database 'data.db'
@@ -163,25 +174,25 @@ def load_a_table_from_database(table_name_or_query: str) -> pd.DataFrame:
 
 
 def load_manual_gapfill(tablepath: str='data/manual_curation.xlsx' , sheet_name: str='gapfill') -> pd.DataFrame:
-    """loads gapfill sheet from manual curation table
+    """Loads gapfill sheet from manual curation table
 
     Args:
-        tablepath (str): Path to manual curation table. Defaults to 'data/manual_curation.xlsx'.
-        sheet_name (str): Sheet name for reaction gapfilling. Defaults to 'gapfill'.
+        - tablepath (str): Path to manual curation table. Defaults to 'data/manual_curation.xlsx'.
+        - sheet_name (str): Sheet name for reaction gapfilling. Defaults to 'gapfill'.
 
     Returns:
-        df: table as pandas df
+        pd.DataFrame: Table containing sheet with name 'gapfill'|specified sheet_name from Excel file
     """
     man_gapf = pd.read_excel(tablepath, sheet_name)
     return man_gapf
 
 
-def write_to_file(model: Model, new_filename: str):
+def write_to_file(model: libModel, new_filename: str):
     """Writes modified model to new file
 
     Args:
-        model (libsbml-model): model loaded with libsbml
-        new_filename (Str): filename / path for modified model
+        - model (libModel): Model loaded with libSBML
+        - new_filename (str): Filename|Path for modified model
     """
     new_document = model.getSBMLDocument()
     writeSBMLToFile(new_document, new_filename)
@@ -192,20 +203,22 @@ def write_report(dataframe: pd.DataFrame, filepath: str):
     """Writes reports stored in dataframes to xlsx file
 
     Args:
-        dataframe (pd.DataFrame): table containing output
-        filepath (str): path to file with filename
+        - dataframe (pd.DataFrame): Table containing output
+        - filepath (str): Path to file with filename
     """
     writer = pd.ExcelWriter(str(os.path.abspath('.')) + '/' + filepath)
     dataframe.to_excel(writer)
     writer.save()
 
 
-def validate_libsbml_model(model: Model):
-    ''' Debug method: Validates a libSBML model with the libSBML validator
+def validate_libsbml_model(model: libModel) -> int:
+    """Debug method: Validates a libSBML model with the libSBML validator
     Args:
-        Args:
-            - model (Model): A libSBML Model
-    '''
+        - model (libModel): A libSBML model
+        
+    Returns:
+        int: Integer specifying if vaidate was successful or not
+    """
     validator = SBMLValidator()
     doc = model.getSBMLDocument()
     
@@ -214,15 +227,19 @@ def validate_libsbml_model(model: Model):
 
 def parse_fasta_headers(filepath: str, id_for_model: bool=False) -> pd.DataFrame:
     """Parses FASTA file headers to obtain:
-        - the protein_id
-        - and the model_id (like it is obtained from CarveMe)
+    
+            - the protein_id
+            - and the model_id (like it is obtained from CarveMe)
+            
         corresponding to the locus_tag
         
-        Args:
-            filepath (str): Path to FASTA file
-            
-        Returns:
-            a pandas dataframe containing the columns locus_tag, Protein_id & Model_id
+    Args:
+        - filepath (str): Path to FASTA file
+        - id_for_model (bool): True if model_id similar to autogenerated 
+            GeneProduct ID should be contained in resulting table
+        
+    Returns:
+        pd.DataFrame: Table containing the columns locus_tag, Protein_id & Model_id
     """
     keyword_list = ['protein', 'locus_tag']
     tmp_dict = dict()
@@ -271,14 +288,14 @@ def parse_fasta_headers(filepath: str, id_for_model: bool=False) -> pd.DataFrame
     return pd.DataFrame(locus2ids)
 
 
-def search_ncbi_for_gpr(locus):
-    """fetches protein name from NCBI
+def search_ncbi_for_gpr(locus: str) -> str:
+    """Fetches protein name from NCBI
 
     Args:
-        locus (string): NCBI compatible locus_tag
+        - locus (str): NCBI compatible locus_tag
 
     Returns:
-        str: protein name / description
+        str: Protein name|description
     """
     handle = Entrez.efetch(
         db="protein",
@@ -296,14 +313,14 @@ def search_ncbi_for_gpr(locus):
                     return record.description, feature.qualifiers["locus_tag"][0]
 
 
-def parse_gff_for_gp_info(gff_file):
+def parse_gff_for_gp_info(gff_file: str) -> pd.DataFrame:
     """Parses gff file of organism to find gene protein reactions based on locus tags
 
     Args:
-        gff_file (Str): path to gff file of organism of interest
+        - gff_file (str): Path to gff file of organism of interest
 
     Returns:
-        df: table containing mapping from locus tag to GPR
+        pd.DataFrame: Table containing mapping from locus tag to GPR
     """
     db = gffutils.create_db(
         gff_file,
@@ -336,13 +353,13 @@ def parse_gff_for_gp_info(gff_file):
     return mapping_df.drop('Parent', axis=1)
 
 def search_sbo_label(sbo_number: str) -> str:
-    """looks up the SBO label corresponding to a given SBO Term number
+    """Looks up the SBO label corresponding to a given SBO Term number
 
     Args:
-        sbo_number (str): Last three digits of SBO-Term as str
+        - sbo_number (str): Last three digits of SBO-Term as str
 
     Returns:
-        str: denoted label for given SBO Term
+        str: Denoted label for given SBO Term
     """
     sbo_number = str(sbo_number)
     client = EBIClient()
@@ -350,15 +367,15 @@ def search_sbo_label(sbo_number: str) -> str:
     return sbo['_embedded']['terms'][0]['label']
 
 
-def save_user_input(configpath):
+def save_user_input(configpath: str) -> dict[str: str]:
     """This aims to collect user input from the command line to create a config file, 
     will also save the user input to a config if no config was given
 
     Args:
-        configpath (str): path to config file if present
+        - configpath (str): Path to config file if present
         
     Returns:
-        dict: either loaded config file or created from user input
+        dict: Either loaded config file or created from user input
     """
     if os.path.isfile(configpath):
         with open(configpath) as f:
