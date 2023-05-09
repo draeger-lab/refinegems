58c0fa48a99ed22337f4822648d4ab17400ff9ae
famosab <famke.baeuerle@gmail.com>
2023-05-09 16:49:05 +0200

    prepare for public release with docs

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 # needed for installation of refinegems
 from setuptools import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setup(name='refineGEMs',
-      version='1.0',
-      description='Scripts to curate a GEM',
-      author='Famke Baeuerle',
+      version='1.0.1',
+      description='refineGEMs is a python package inteded to help with the curation of genome-scale metabolic models (GEMS)',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      author='Famke Baeuerle and Gwendolyn O. Gusak',
       author_email='famke.baeuerle@gmail.com',
+      url='https://github.com/draeger-lab/refinegems',
       license='MIT',
       packages=['refinegems'],
       install_requires = [
@@ -15,7 +21,6 @@ setup(name='refineGEMs',
             "memote==0.13.0",
             "pandas==1.2.4",
             "numpy==1.20.3",
-            # "pyyaml==5.4.1",
             "gffutils==0.10.1",
             "markupsafe==2.0.1", 
             "depinfo==1.7.0",
