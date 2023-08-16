23e307fb0db4d32696c628a8c5639f4e8cbb7940
Gwendolyn O. Gusak <81755070+GwennyGit@users.noreply.github.com>
2023-08-16 14:55:05 +0100

    Merge hot fix for polish into main (#96)
    * Changed PyPI version badge
    
    For the next release the PyPI version badge stems now from 'shields.io' and not from 'badge.fury.io'.
    
    * Changed colour for refineGEMs version badge
    
    * Adjusted handling of BioCyc identifiers in polish_annotations #95 #58
    
    * Added requirement for importlib_resources=5.13.0 to Pipfile
    
    * Added code to cope with missing sub-database prefixes for BioCyc identifiers #95
    
    * Changed NaN identifier handling #95
    
    * Fixed issue III: None prefix identifier pairs in invalid_curies.tsv #95
    
    * Adjusted files with version for release 1.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ project_copyright = '2023, Famke Bäuerle and Gwendolyn O. Gusak'
 author = 'Famke Bäuerle and Gwendolyn O. Gusak'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.1'
+release = '1.2.2'
 
 
 # -- General configuration ---------------------------------------------------
