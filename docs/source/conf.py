b8ab57c5a2893a7f82fff3a1fec5d37a093f2d18
Gwendolyn O. Gusak <81755070+GwennyGit@users.noreply.github.com>
2023-08-21 17:17:41 +0100

    Updated files with new version 1.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ project_copyright = '2023, Famke Bäuerle and Gwendolyn O. Gusak'
 author = 'Famke Bäuerle and Gwendolyn O. Gusak'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.2'
+release = '1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
