aa0a2f017a135238954823263cb45065493276d9
cb-Hades <81743695+cb-Hades@users.noreply.github.com>
2023-10-31 13:41:01 +0100

    Update medium.py
    - function for uploading new medium fixed
    - added safety measures for upload
    --> note: avoid hard shutdown for add_medium
    
    part of issue #49

--- refinegems/database/medium.py
+++ refinegems/database/medium.py
@@ -10,6 +10,7 @@ __author__ = "Carolin Brune"
 ############################################################################
 
 ALLOWED_DATABASE_LINKS = ['BiGG', 'MetaNetX', 'SEED', 'ChEBI', 'KEGG']
+REQUIRED_SUBSTANCE_ATTRIBUTES = ['name', 'formula', 'flux', 'source']
 
 ############################################################################
 # classes
@@ -18,16 +19,24 @@ ALLOWED_DATABASE_LINKS = ['BiGG', 'MetaNetX', 'SEED', 'ChEBI', 'KEGG']
 
 class Medium:
 
-    def __init__(self, name:str, substance_table:pd.DataFrame, description=None):
+    def __init__(self, name:str, substance_table:pd.DataFrame, description=None, doi=None):
         
         self.name = name
         self.description = description
         self.substance_table = substance_table
+        self.doi = doi
+
+    # -----------------------------------
+    # TODO:
+    # add functionalities from SPECIMEN ?
+    # ------------------------------------
+
 
 ############################################################################
 # functions for loading from DB
 ############################################################################
 
+# TODO
 def load_substance_table_from_db(mediumname: str, database:str, type:str) -> pd.DataFrame:
     
     # build connection to DB
@@ -88,13 +97,18 @@ def load_medium_from_db(name:str, database:str, type:str) -> Medium:
         print('ERROR: Unknown medium name', file=sys.stderr)
         sys.exit(1)
 
+    # get DOI(s)
+    result = cursor.execute("SELECT medium.reference FROM medium WHERE medium.name = ?",(name,))
+    doi = result.fetchone()[0]
+
     # close connection to DB
     connection.close()
     
     # get substance table
     substance = load_substance_table_from_db(name, database, type)
 
-    return Medium(name=name, substance_table=substance, description=description)
+    return Medium(name=name, substance_table=substance, description=description, doi=doi)
+
 
 ############################################################################
 # functions for reading media from extern
@@ -116,15 +130,18 @@ def read_substances_from_file(path: str) -> pd.DataFrame:
     # read in the table
     substance_table = pd.read_csv(path, sep='\t')
 
-    # ............................................
+    # validate the table
+    substance_cols = substance_table.columns
+    for req in REQUIRED_SUBSTANCE_ATTRIBUTES:
+        if req in substance_cols:
+            continue
+        else:
+            substance_table[req] = None
 
-    # TODO
-
-    # name, formula, flux and source HAVE to exist
-
-    # filter out unwanted database IDs
-
-    # ............................................
+    # remove unwanted database links
+    all_allowed = REQUIRED_SUBSTANCE_ATTRIBUTES + ALLOWED_DATABASE_LINKS
+    for_removal = [_ for _ in substance_cols if _ not in all_allowed] 
+    substance_table.drop(for_removal, axis=1, inplace=True)
 
     # convert all NaNs into None
     substance_table = substance_table.replace({np.nan: None})
@@ -144,13 +161,16 @@ def read_external_medium(how:str) -> Medium:
             # prompt for description
             description = input('Enter a short description for the medium:\n')
 
+            # promt for description 
+            reference = input('Enter the reference linke (DOI) for the medium:\n')
+
             # prompt for path to substance table
             substance_path = input('Enter a path to a table of substances (TSV):\n')
             # tsv format
             substances = read_substances_from_file(substance_path)
 
             # construct medium
-            new_medium = Medium(name=name, description=description, substance_table=substances)
+            new_medium = Medium(name=name, description=description, substance_table=substances, doi=reference)
 
             return new_medium
         
@@ -169,37 +189,71 @@ def read_external_medium(how:str) -> Medium:
 # functions for adding entries to DB
 ############################################################################
 
+def get_last_idx_table(tablename, connection, cursor):
+
+    match tablename:
+        case 'medium':
+            last_rowid_res = cursor.execute("SELECT MAX(rowid) FROM medium")
+        case 'medium2substance':
+            last_rowid_res = cursor.execute("SELECT MAX(rowid) FROM medium2substance")
+        case 'substance':
+            last_rowid_res = cursor.execute("SELECT MAX(rowid) FROM substance")
+        case 'substance2db':
+            last_rowid_res = cursor.execute("SELECT MAX(rowid) FROM substance2db")
+        case _:
+            sys.exit(f'Unknown table name for database: {tablename}')
+
+    last_rowid_fetched = last_rowid_res.fetchone()
+    last_rowid = last_rowid_fetched[0]
+
+    return last_rowid
+
+
 def enter_substance_row(row: pd.Series, connection, cursor):
 
-    candidates = cursor.execute("SELECT * FROM substance WHERE substance.formula = ? OR substance.name LIKE ? ",(row['formula'],row['name']))
-    candidate_list = candidates.fetchall()
+    # check if a perfect match exists in database
+    exact_match_res = cursor.execute("SELECT * FROM substance WHERE substance.formula = ? AND substance.name = ? ",(row['formula'],row['name']))
+    exact_match = exact_match_res.fetchone()
 
-    # if candidates were found, let user chose if one of those should be used
-    substance_id = None
-    if len(candidate_list) > 0:
-        print(f'Following similar entries have been found for the substance {row["name"]}.')
-        for i in candidate_list:
-            print(i)
-        res = input('If one matches your entry, please enter the ID or enter skip/s: \n')
-        while True:
-            # skip
-            if res in ['skip','s']:
-                break
-            # match found
-            elif res.isnumeric() and int(res) in [_[0] for _ in candidate_list]:
-                substance_id = int(res)
-                break
-            # unknown input, try again
-            else:
-                input('Unknow input. Please try again:\n')
+    if exact_match:
 
-    # if no matching entry has been found, insert new entry into DB
-    if not substance_id:
-        cursor.execute("INSERT INTO substance VALUES(?,?,?)",(None,row['name'],row['formula'],))
-        connection.commit()
-        # retrieve the newly added ID
-        res = cursor.execute("SELECT last_insert_rowid() FROM substance")
-        substance_id = res.fetchone()[0]
+        # get substance id
+        substance_id = exact_match[0]
+
+    # if not promt for similar or create new substance
+    else:    
+
+        candidates = cursor.execute("SELECT * FROM substance WHERE substance.formula = ? OR substance.name LIKE ? ",(row['formula'],row['name']))
+        candidate_list = candidates.fetchall()
+
+        # if candidates were found, let user chose if one of those should be used
+        
+        substance_id = None
+        if len(candidate_list) > 0:
+
+            print(f'Following similar entries have been found for the substance {row["name"]}.')
+            for i in candidate_list:
+                print(i)
+            res = input('If one matches your entry, please enter the ID or enter skip/s: \n')
+            while True:
+                # skip
+                if res in ['skip','s']:
+                    break
+                # match found
+                elif res.isnumeric() and int(res) in [_[0] for _ in candidate_list]:
+                    substance_id = int(res)
+                    break
+                # unknown input, try again
+                else:
+                    res = input('Unknow input. Please try again:\n')
+
+        # if no matching entry has been found, insert new entry into DB
+        if not substance_id:
+            cursor.execute("INSERT INTO substance VALUES(?,?,?)",(None,row['name'],row['formula'],))
+            connection.commit()
+            # retrieve the newly added ID
+            res = cursor.execute("SELECT last_insert_rowid() FROM substance")
+            substance_id = res.fetchone()[0]
         
     return substance_id
 
@@ -228,70 +282,86 @@ def enter_medium_into_db(database: str, medium: Medium):
     connection = sqlite3.connect(database)
     cursor = connection.cursor()
 
-    # name
-    #   check, if name already in DB and prompt, if that problem occurs
-    #   enter into database, if no problem remains 
-    name_check_res = cursor.execute("SELECT 1 FROM medium WHERE medium.name = ?", (medium.name,))
-    name_check = name_check_res.fetchone()
-    if not name_check:
-        # medium name is unique 
-        # add medium to DB
-        cursor.execute("INSERT INTO medium VALUES(?,?,?)",(None,medium.name,medium.description,))
-    else:
-        # medium name already in DB 
-        check = input('The name {medium.name} is already in the database.\nDo you want to set a new name? (yes/no): ')
-        if check in ['y','yes']:
-            # set a new name
-            while True:
-                new_name = input('Please enter a medium name:\n')
-                name_check_2 = cursor.execute("SELECT 1 FROM medium WHERE medium.name = ?", (new_name,))
-                if name_check_2.fetchone():
-                    print('This name already exists in the database.')
-                else:
-                    medium.name = new_name
-                    break
-        elif check in ['n','no']:
-            # end program when no new name is set
-            print('No new name chosen. Ending the program.')
-            sys.exit()
+    # collect starting IDs
+    sid_medium = get_last_idx_table('medium', connection, cursor)
+    sid_medium2substance = get_last_idx_table('medium2substance', connection, cursor)
+    sid_substance = get_last_idx_table('substance', connection, cursor)
+    sid_substance2db = get_last_idx_table('substance2db', connection, cursor)
+
+    # try to add a new medium to the database
+    try:
+
+        # name
+        #   check, if name already in DB and prompt, if that problem occurs
+        #   enter into database, if no problem remains 
+        name_check_res = cursor.execute("SELECT 1 FROM medium WHERE medium.name = ?", (medium.name,))
+        name_check = name_check_res.fetchone()
+        if not name_check:
+            # medium name is unique 
+            # add medium to DB
+            cursor.execute("INSERT INTO medium VALUES(?,?,?,?)",(None,medium.name,medium.description,medium.doi,))
         else:
-            # Abort program at unknown input
-            sys.exit('Unknown input. Aborting.')
-    
-    connection.commit()
-    res = cursor.execute("SELECT last_insert_rowid() FROM medium")
-    medium_id = res.fetchone()[0]
+            # medium name already in DB 
+            check = input('The name {medium.name} is already in the database.\nDo you want to set a new name? (yes/no): ')
+            if check in ['y','yes']:
+                # set a new name
+                while True:
+                    new_name = input('Please enter a medium name:\n')
+                    name_check_2 = cursor.execute("SELECT 1 FROM medium WHERE medium.name = ?", (new_name,))
+                    if name_check_2.fetchone():
+                        print('This name already exists in the database.')
+                    else:
+                        medium.name = new_name
+                        break
+            elif check in ['n','no']:
+                # end program when no new name is set
+                print('No new name chosen. Ending the program.')
+                sys.exit()
+            else:
+                # Abort program at unknown input
+                sys.exit('Unknown input. Aborting.')
+        
+        connection.commit()
+        res = cursor.execute("SELECT last_insert_rowid() FROM medium")
+        medium_id = res.fetchone()[0]
 
-    # substances 
-    medium.substance_table['substance_id'] = medium.substance_table.apply(enter_substance_row, axis=1, args=(connection, cursor))
+        # substances 
+        medium.substance_table['substance_id'] = medium.substance_table.apply(enter_substance_row, axis=1, args=(connection, cursor))
 
-    # medium2substances
-    medium.substance_table.apply(enter_m2s_row, axis=1, args=(medium_id, connection, cursor))
+        # medium2substances
+        medium.substance_table.apply(enter_m2s_row, axis=1, args=(medium_id, connection, cursor))
 
-    # substance2db
-    avail_data = [_ for _ in medium.substance_table.columns if _ in ALLOWED_DATABASE_LINKS]
-    for current_db in avail_data:
-        medium.substance_table.apply(enter_s2db_row, axis=1, args=(current_db,connection,cursor))
+        # substance2db
+        avail_data = [_ for _ in medium.substance_table.columns if _ in ALLOWED_DATABASE_LINKS]
+        for current_db in avail_data:
+            medium.substance_table.apply(enter_s2db_row, axis=1, args=(current_db,connection,cursor))
 
-    # commit changes
-    # connection.commit()
-    connection.close()
+        print(f'Medium {medium.name} with ID {medium_id} has been added to the database.')
 
-    print(f'Medium {medium_id} has been added to the database.')
+    # in case of problems, interupt and revert changes
+    except:
+
+        print(f'During execution, the following error occured: \n{sys.exc_info()}')
+        print('Reverting changes to database...')
+
+        cursor.execute("DELETE FROM medium WHERE rowid > ?",(sid_medium,))
+        cursor.execute("DELETE FROM medium WHERE rowid > ?",(sid_medium2substance,))
+        cursor.execute("DELETE FROM medium WHERE rowid > ?",(sid_substance,))
+        cursor.execute("DELETE FROM medium WHERE rowid > ?",(sid_substance2db,))
+
+        print('Done')
+
+    # in any case close connection to database
+    finally:
+        connection.close()
 
 
 ############################################################################
 # possible entry points
 ############################################################################
 
-# ..........................................................................
-# WARNING AND TODO:
-# currently, when the functions raises an error half-way through the insertions
-# THEY STAY!!!
-# -> maybe work on a temp file or delete everything added in case of a fail?
-# ..........................................................................
-
 # entry point for entering a medium using the command line
+# TODO since database is part of package, direct accessing possible
 def add_medium(database:str):
     
     # get external medium
