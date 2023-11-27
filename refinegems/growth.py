e695cd61a4d4cbf81c786e34e43890da6c48d4c0
cb-Hades <81743695+cb-Hades@users.noreply.github.com>
2023-11-27 10:16:41 +0100

    Update growth.py
    delete old functions

--- refinegems/growth.py
+++ refinegems/growth.py
@@ -347,83 +347,16 @@ def growth_sim_multi(models: cobraModel|list[cobraModel], media: medium.Medium|l
 # -> run simulation 
 # -> visulise also here?
 def growth_analysis():
+
+    # read-in / collect all models into list
+    models = None
+    # create / collect all media into list
+    media = None
+    # run simulation
+    report = growth_sim_multi(models, media)
+    # save / visualise report 
     pass
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# some old function to recheck, if everything still fits
-"""
-
-# entkoppelt
-# ??????????
-# just deleted from growth - rewrite?
-
-
-# recheck
-def get_growth_selected_media(model: cobraModel, media: list[str], basis: str, anaerobic: bool) -> pd.DataFrame:
-    Simulates growth on all given media
-
-    Args:
-        - model (cobraModel): Model loaded with COBRApy
-        - media (list[str]): Ids of media to simulate on
-        - basis (str): Either default_uptake (adding metabs from default) or minimal_uptake (adding metabs from minimal medium)
-        - anaerobic (bool): If True 'EX_o2_e' is set to 0.0 to simulate anaerobic conditions
-
-    Returns:
-        pd.DataFrame: Information on growth behaviour on given media
-    
-    growth = pd.DataFrame()
-    for medium in media:
-        medium_df = load_medium_from_db_for_growth(medium)
-        if (basis == 'default_uptake'):
-            growth_one = growth_one_medium_from_default(model, medium_df, anaerobic)
-        elif (basis == 'minimal_uptake'):
-            growth_one = growth_one_medium_from_minimal(model, medium_df, anaerobic)
-        growth = pd.concat([growth, growth_one], ignore_index=True)
-    return growth
-
-
-def simulate_all(models: list[cobraModel], media: list[str], basis: str, anaerobic: bool) -> pd.DataFrame:
-    Does a run of growth simulation for multiple models on different media
-
-    Args:
-        - models (list[cobraModel]): Models loaded with cobrapy
-        - media (list[str]): Media of interest (f.ex. LB, M9, ...)
-        - basis (str): Either default_uptake (adding metabs from default) or minimal_uptake (adding metabs from minimal medium)
-        - anaerobic (bool): If True 'EX_o2_e' is set to 0.0 to simulate anaerobic conditions
-
-    Returns:
-        pd.DataFrame: table containing the results of the growth simulation
-    
-    growth = pd.DataFrame()
-    for medium_id in tqdm(media):
-        medium = load_medium_from_db(medium_id)
-        for model in models:
-            essentials_given = False
-            if (basis=='default_uptake'):
-                growth_one = growth_one_medium_from_default(model, medium, anaerobic).drop('missing exchanges', axis=1)
-            elif (basis == 'minimal_uptake'):
-                growth_one = growth_one_medium_from_minimal(model, medium, anaerobic).drop('missing exchanges', axis=1)
-            if growth_one['essential'].dropna().size == 0:
-                essentials_given = True
-            else:
-                growth_list = growth_one['essential'].dropna().to_list()
-                growth_string = ', '.join(growth_list)
-                essentials_given = growth_string
-            growth_one = growth_one.drop('essential', axis=1)
-            growth_one['complete'] = essentials_given
-            growth_one = growth_one.dropna()
-            growth_one['model'] = model.id
-            growth_one = growth_one[['model', 'medium', 'doubling_time [min]', 'growth_value', 'complete']]
-            growth_one['doubling_time [min]'].astype(float).round(2)
-            growth_one['growth_value'].astype(float).round(2)
-            growth = growth.append(
-                growth_one, 
-                ignore_index=True)
-
-    return growth
-"""
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # @RENAMED
 def get_essential_reactions_via_single_knockout(model: cobraModel) -> list[str]:
