1921adf5d20f6edd3aa3cc2ff8494f2bd7b6db4e
cb-Hades <81743695+cb-Hades@users.noreply.github.com>
2023-11-27 09:54:48 +0100

    Update growth.py, reports.py
    + added functions for checking for essential exchanges and returning those informations
    + minor restructuring and documentation changes
    + moved heatmap plotting to reports, put not yet usable

--- refinegems/reports.py
+++ refinegems/reports.py
@@ -7,6 +7,11 @@ __author__ = 'Carolin Brune'
 # requirements
 ################################################################################
 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 ################################################################################
 # classes
@@ -31,6 +36,15 @@ class SingleGrowthSimulationReport(Report):
         self.additives = additives
         self.no_exchange = no_exchange
 
+    def to_dict(self) -> dict:
+
+        return {'model_name': self.model_name,
+                 'medium_name': self.medium_name,
+                 'growth_value': self.growth_value,
+                 'doubling_time': self.doubling_time,
+                 'additives': self.additives if len(self.additives) > 0 else None,
+                 'no_exchange': self.no_exchange}
+
 
 class GrowthSimulationReport(Report):
 
@@ -39,5 +53,116 @@ class GrowthSimulationReport(Report):
         self.reports = reports
 
     def add_sim_results(self, new_rep: SingleGrowthSimulationReport):
-        
+
         self.reports.append(new_rep)
+
+    def to_table(self):
+
+        l = []
+        for report in self.reports:
+            l.append(report.to_dict())
+        return pd.DataFrame(l)
+
+
+    def plot_heatmap_dt(self):
+        """Creates heatmap of simulated doubling times with additives
+        
+        Args:
+            - growth (pd.DataFrame): Containing growth data from simulate_all
+            
+        Returns:
+            plot: Seaborn Heatmap
+        """
+        growth=growth.set_index(['medium', 'model']).sort_index().T.stack()
+        growth.columns.name=None
+        growth.index.names = (None,None)
+        growth.index.name=None
+        growth.index = growth.index.get_level_values(1)
+        growth[growth > 500] = 0
+        growth[growth < 0] = 0
+        growth.replace([np.inf, -np.inf], 0, inplace=True)
+        over_growth = growth.max().max() + 6
+        growth.replace(np.nan, over_growth, inplace=True)
+        under_growth = growth.min().min() - 5
+        vmin= under_growth if under_growth > 1e-5 else 1e-5 #Use same threshhold as in find_missing_essential in growth
+        vmax=over_growth - 1
+        annot = growth.copy()
+        annot = annot.round().astype(int)
+        annot[annot < 1e-5] = ''
+        annot.replace(over_growth.round().astype(int), 'No data', inplace=True)
+        cmap=matplotlib.cm.get_cmap('YlGn').copy()
+        cmap.set_under('black')
+        cmap.set_over('white')
+        fig, ax = plt.subplots(figsize=(10,8))
+        sns.heatmap(growth.T, 
+                    annot=annot.T, 
+                    annot_kws={"fontsize":15},
+                    vmin=vmin, 
+                    vmax=vmax,
+                    cmap=cmap, 
+                    linewidth=.5, 
+                    cbar_kws = {'orientation':'vertical', 'label':'doubling time [min]', 'extend': 'min', 'extendrect':True},
+                    ax=ax,
+                    fmt=''
+                    )
+        rotation = 40 if len(growth.index) > 3 else 0
+        plt.tick_params(rotation=0, bottom=False, top=False, left=False, right=False)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=rotation, ha="right")
+        return fig
+
+    def plot_heatmap_native(growth: pd.DataFrame):
+        """Creates a plot were if growth without additives is possible is marked from yellow to green otherwise black
+
+        Args:
+            - growth (pd.DataFrame): Containing growth data from simulate_all
+            
+        Returns:
+            plot: Seaborn Heatmap
+        """
+        def get_native_growth(row):
+            if row['complete'] == True:
+                return row['doubling_time [min]']
+            else:
+                return 0
+        
+        growth['native_growth'] = growth.apply(get_native_growth, axis=1)
+        growth = growth[['medium', 'model', 'native_growth']]
+        growth=growth.set_index(['medium', 'model']).sort_index().T.stack()
+        growth.columns.name=None
+        growth.index.names = (None,None)
+        growth.index.name=None
+        growth.index = growth.index.get_level_values(1)
+        growth[growth > 500] = 0
+        growth[growth < 0] = 0
+        growth.replace([np.inf, -np.inf], 0, inplace=True)
+        over_growth = growth.max().max() + 6
+        growth.replace(np.nan, over_growth, inplace=True)
+        annot = growth.copy()
+        annot = annot.round().astype(int)
+        annot[annot == np.nan] = 'No data'
+        annot[annot < 1e-5] = ''
+        annot.replace(over_growth.round().astype(int), 'No data', inplace=True)
+        under_growth = growth.min().min() - 5
+        vmin= under_growth if under_growth > 1e-5 else 1e-5 #Use same threshhold as in find_missing_essential in growth
+        vmax= over_growth - 1
+        cmap=matplotlib.cm.get_cmap('YlGn').copy()
+        cmap.set_under('black')
+        cmap.set_over('white')
+        fig, ax = plt.subplots(figsize=(10,8))
+        sns.heatmap(growth.T,
+                    annot=annot.T, 
+                    annot_kws={"fontsize":15},
+                    vmin=vmin,
+                    vmax=vmax,
+                    cmap=cmap,
+                    linewidth=.5, 
+                    ax=ax,
+                    cbar_kws={'orientation':'vertical', 'label':'doubling time [min]', 'extend': 'min', 'extendrect':True},
+                    fmt=''
+                    )
+        plt.xticks(rotation=0)
+        plt.yticks(rotation=0)
+        rotation = 40 if len(growth.index) > 3 else 0
+        plt.tick_params(rotation=0, bottom=False, top=False, left=False, right=False)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=rotation, ha="right")
+        return fig
