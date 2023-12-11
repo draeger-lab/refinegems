5053301f853b7c24d6b36db1a6bbadf2532f6b4a
cb-Hades <81743695+cb-Hades@users.noreply.github.com>
2023-12-11 09:39:10 +0100

    Deleted simulate_all from comparison

--- refinegems/comparison.py
+++ refinegems/comparison.py
@@ -13,8 +13,7 @@ from tqdm import tqdm
 from venn import venn
 from libsbml import Model as libModel
 from cobra import Model as cobraModel
-from refinegems.io import load_medium_from_db, search_sbo_label
-from refinegems.growth import growth_one_medium_from_default, growth_one_medium_from_minimal
+from refinegems.io import search_sbo_label
 from refinegems.investigate import initial_analysis, get_reactions_per_sbo
 
 __author__ = "Famke Baeuerle"
@@ -214,45 +213,4 @@ def plot_heatmap_native(growth: pd.DataFrame):
     plt.tick_params(rotation=0, bottom=False, top=False, left=False, right=False)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=rotation, ha="right")
     return fig
-
-# @NOTE: moved to growth + overhauled there
-def simulate_all(models: list[cobraModel], media: list[str], basis: str, anaerobic: bool) -> pd.DataFrame:
-    """Does a run of growth simulation for multiple models on different media
-
-    Args:
-        - models (list[cobraModel]): Models loaded with cobrapy
-        - media (list[str]): Media of interest (f.ex. LB, M9, ...)
-        - basis (str): Either default_uptake (adding metabs from default) or minimal_uptake (adding metabs from minimal medium)
-        - anaerobic (bool): If True 'EX_o2_e' is set to 0.0 to simulate anaerobic conditions
-
-    Returns:
-        pd.DataFrame: table containing the results of the growth simulation
-    """
-    growth = pd.DataFrame()
-    for medium_id in tqdm(media):
-        medium = load_medium_from_db(medium_id)
-        for model in models:
-            essentials_given = False
-            if (basis=='default_uptake'):
-                growth_one = growth_one_medium_from_default(model, medium, anaerobic).drop('missing exchanges', axis=1)
-            elif (basis == 'minimal_uptake'):
-                growth_one = growth_one_medium_from_minimal(model, medium, anaerobic).drop('missing exchanges', axis=1)
-            if growth_one['essential'].dropna().size == 0:
-                essentials_given = True
-            else:
-                growth_list = growth_one['essential'].dropna().to_list()
-                growth_string = ', '.join(growth_list)
-                essentials_given = growth_string
-            growth_one = growth_one.drop('essential', axis=1)
-            growth_one['complete'] = essentials_given
-            growth_one = growth_one.dropna()
-            growth_one['model'] = model.id
-            growth_one = growth_one[['model', 'medium', 'doubling_time [min]', 'growth_value', 'complete']]
-            growth_one['doubling_time [min]'].astype(float).round(2)
-            growth_one['growth_value'].astype(float).round(2)
-            growth = growth.append(
-                growth_one, 
-                ignore_index=True)
-
-    return growth
 
