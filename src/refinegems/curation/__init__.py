d747386b40edffe4a2ffa4ead991ed77b80886ac
cb-Hades <81743695+cb-Hades@users.noreply.github.com>
2024-08-20 14:03:27 +0200

    update

--- src/refinegems/curation/__init__.py
+++ src/refinegems/curation/__init__.py
@@ -1,10 +1,9 @@
-__all__ = ['db_access','biomass','charges','curate','gapfill','pathways','polish']
+__all__ = ['db_access','biomass','charges','curate','pathways','polish']
 
 from . import db_access
 
 from . import biomass
 from . import charges
 from . import curate 
-from . import gapfill
 from . import pathways
 from . import polish
